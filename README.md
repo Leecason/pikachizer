a856d69508d39d393d58b9f660e2e54bd4db8f65
leecason <leecason0722@gmail.com>
2020-07-10 21:06:26 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Pikachizer
+
+一个 JavaScript 版的皮卡丘语翻译器 =w=
+
+Translate some text to the mysterious language spoken by Pikachu! =w=
+
+## Usage
+
+```js
+const pikachizer = require('pikachizer');
+
+pikachizer.translate('Hello World!') // Pikachu!
+```
---
+++ pikachizer.js
@@ -0,0 +1,65 @@
+const PIKACHU = [
+  ['pi', 'pii'],
+  ['pi', 'ka', 'chu'],
+  ['pii', 'pi-i', 'ka', 'kaa', 'chu'],
+  ['pika', 'piii', 'pipi', 'kachu', 'kaaa', 'chuu'],
+  ['pikaa', 'pikapi', 'kachu', 'piika', 'chuuu'],
+  ['piikaa', 'pikachu', 'chuuuu', 'pika-pi', 'kaa-piikaa-pi'],
+  ['pika-chu', 'pika-pika', 'piikachu', 'pikachu'],
+  ['pika-pika', 'piikachu', 'pi-kaaa-chu', 'pi-pikachu'],
+];
+
+const cache = {};
+
+// [min, max)
+function getRandom(min, max) {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+function isUpperFirst(character) {
+  return /[A-Z]/.test(character.charAt(0));
+}
+
+function upperFirst(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+function pika (word) {
+  if (cache[word]) return cache[word];
+
+  let new_word;
+  const len = word.length;
+  if (len < 8) {
+    const arr = PIKACHU[getRandom(0, PIKACHU.length)];
+    new_word = arr[getRandom(0, arr.length)];
+  } else {
+    const split = getRandom(1, len);
+    new_word = `${pika(word.substr(0, split))-pika(word.substr(split))}`;
+  }
+
+  cache[word] = new_word;
+  return new_word;
+}
+
+function pikachize (contents) {
+  let prev_word_end_index = 0;
+  let res = '';
+  const matches = contents.matchAll(/\b\w+\b/g);
+  for (const match of matches) {
+    const word = match[0];
+    const word_start_index = match.index;
+    const spacer = contents.substring(prev_word_end_index, word_start_index);
+    prev_word_end_index = word_start_index + word.length;
+
+    const new_word = pika(word);
+    res += `${spacer}${isUpperFirst(word) ? upperFirst(new_word) : new_word}`;
+  }
+  res += contents.substring(prev_word_end_index);
+  return res;
+}
+
+const pikachizer = {
+  translate: pikachize,
+}
+
+module.exports = pikachizer;
